de99d632fef2de6813d643b4d5fe427ac456ddf6
tdoshi123 <168225392+tdoshi123@users.noreply.github.com>
2024-08-28 14:17:43 -0400

    Update Lab2_TD.cpp

--- Lab2_TD.cpp
+++ Lab2_TD.cpp
@@ -1,20 +1,29 @@
-// Lab2_TD.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab2_TD.cpp : This file contains the 'main' function. Program execution begins and ends there. //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float A;
+    float B;
+    float X;
+
+    cout << "Hello, my name is Tirth Doshi and I'm going to solve the equation:\nAx + B = 0\nFor x\n";
+    
+    cout << "\nPlease enter a value for A: ";
+    cin >> A;
+
+    cout << "A = " << A << endl;
+
+    cout << "\nPlease enter a value for B: ";
+    cin >> B;
+
+    cout << "B = " << B << endl;
+
+    cout << "Solving " << A << "x + " << B << " = 0 For x..." << endl;
+
+    X = (0 - B) / A;
+        
+    cout << "\nThe answer is: \nx=" << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
